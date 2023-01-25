aabfaff3fa7faadeeed3848b82cc3e6c1050cf6f
emilyallerdings <skittlesanator@gmail.com>
2023-01-24 18:49:40 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from hippocluster.graphs.lattice import RandomWalkLattice
+
 from hippocluster.graphs.environment import RandomWalkEnvironment
-from hippocluster.algorithms.hippocluster import Hippocluster
-from hippocluster.graphs.abstract import RandomWalkGraph
-from hippocluster.graphs.absgraph import AbstractedGraph
+from absgraph import AbstractedGraph
 
 
 graph = RandomWalkEnvironment()
 
-absgraph = AbstractedGraph(graph, 1)
+absgraph = AbstractedGraph(graph, 4)
 
-absgraph.printgraph()
+absgraph.printgraphs(1,3)
