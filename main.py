6221c50fcbb1aec41915963fd63229afdb531b41
echalmers <echalmers@mtroyal.ca>
2023-02-06 19:35:06 -0700

    remove test code

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-import networkx as nx
-
 from table_conversion import t_table_to_graph
 from absgraph import AbstractedGraph
-import tables
 import pickle
 
 with open("t_walls_grid_world.p", "rb") as f:
     T = pickle.load(f)
-
-T.get_random_transition_probabilities_from((45,9))
 
 G = t_table_to_graph(
     T=T,
