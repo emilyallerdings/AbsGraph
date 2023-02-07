b8977b6e452c584151d1578693a1fbebd69ab664
echalmers <echalmers@mtroyal.ca>
2023-02-06 19:49:03 -0700

    add comment

--- table_conversion.py
+++ table_conversion.py
@@ -4,6 +4,13 @@ from hippocluster.graphs.abstract import RandomWalkGraph
 
 
 def t_table_to_graph(T: TTable, pos_calculator: callable = None):
+    """
+    convert a T table (from a model-based RL agent) to a directed graph suitable for random walk generation
+    :param T: the TTable object
+    :param pos_calculator: optional callable that returns (x,y) coordinates for plotting given a state
+    :return a networkx DiGraph object with a node for each state in the T table, and directed edges with weights
+    representing the probability that a random action will cause that transition
+    """
 
     G = nx.DiGraph()
     all_states = T.get_all_states()
